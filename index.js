0f40c0fb442b9e328ba5ca105505b81bbc5f46d1
Patrick Santos <patrickspteixeira@gmail.com>
2020-01-14 08:46:52 -0300

    adiciona eslint + prettier + editorconfig

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3333;
 
-app.get('/', (req, res) => {
-    return res.json({message:'top'});
+app.post('/users', (req, res) => {
+  console.log('req:', req.body);
+  return res.json({ message: 'omnistack top' });
 });
 
 app.listen(PORT);
