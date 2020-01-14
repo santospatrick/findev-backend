013fa0b819f0732581e7ecfbc2558d149ec7bfbe
Patrick Santos <patrickspteixeira@gmail.com>
2020-01-14 09:44:41 -0300

    adiciona geolocalizacao

--- src/routes.js
+++ src/routes.js
@@ -5,22 +5,28 @@ const Dev = require('./models/Dev');
 const routes = Router();
 
 routes.post('/devs', async (req, res) => {
-  const { github_username, techs } = req.body;
+  const { github_username, techs, latitude, longitude } = req.body;
 
   const response = await axios.get(
     `https://api.github.com/users/${github_username}`,
   );
 
-  const { name = login, avatar_url, bio } = response.data;
+  const { name, login, avatar_url, bio } = response.data;
 
   const techsArray = techs.split(',').map(tech => tech.trim());
 
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
   const values = {
-    name,
+    name: name || login,
     github_username,
     avatar_url,
     bio,
     techs: techsArray,
+    location,
   };
 
   const dev = await Dev.create(values);
